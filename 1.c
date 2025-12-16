0391b7da592dca22e8128f3b1545e483fffe7b6b
Mohammed Fahad <167276993+fahad9845@users.noreply.github.com>
2025-12-16 06:21:43 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -2,6 +2,7 @@
 
 int main() {
     printf("Hello, World!\n");          // First print
-    printf("Welcome to C programming"); // Second print
+    printf("Welcome to C programming");
+    printf("NOTHing ");// Second print
     return 0;                           // Program ends successfully
 }
